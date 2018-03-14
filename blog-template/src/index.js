bc692a49c765bd2d67c8a0e71874a75ad5230887
Saptarshi Dey <dey7.kol@gmail.com>
2018-03-14 22:53:05 +0530

    parent js file

---
+++ blog-template/src/index.js
@@ -0,0 +1,7 @@
+//import 'materialize-css/dist/css/materialize.min.css'
+import React from "react";
+import ReactDOM,{render} from "react-dom";
+import App from "./components/App"
+
+render(
+<App />,window.document.getElementById('root'));
