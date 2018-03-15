73c5d2836b6069816cfefa6ee723bb3501746f5a
Saptarshi Dey <dey7.kol@gmail.com>
2018-03-16 00:20:41 +0530

    redesinging navbar and login modal

--- blog-template/src/components/Navbar.js
+++ blog-template/src/components/Navbar.js
@@ -1,16 +1,101 @@
-import React from "react";
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
+import { Button, Header, Image, Modal,Form,Checkbox } from 'semantic-ui-react'
 
-class Navbar extends React.Component{
+class Navbar extends Component {
+
+  
+  state = {}
+
+  handleItemClick = (e, { name }) => {
+      this.setState({ activeItem: name,isModal:true })
+}
+
+  render() {
+    const { activeItem } = this.state
+
+    var signIn = <Menu.Item
+                    name='sign-in'
+                    active={activeItem === 'sign-in'}
+                    onClick={this.handleItemClick}
+                >
+                {'Sign-in'}<i className="fa fa-user" style={{fontSize:"20px",marginLeft:'5px'}}></i>
+                </Menu.Item>
+    
+    if(this.state.isModal==true){
+        var modal = <ModalExample modalNode={signIn}/>
+    }
+    else{
+        var modal = <div></div>
+    }
+
+    return (
+      <Menu stackable>
+        <Menu.Item>
+          <img src='/images/logo.jpg' />
+          AuthorsDaddy
+        </Menu.Item>
+
+                
+
+        <Menu.Item
+          name='testimonials'
+          active={activeItem === 'testimonials'}
+          onClick={this.handleItemClick}
+        >
+          Contact Us <i className="fa fa-phone" style={{fontSize:"20px",marginLeft:'5px'}}></i>
+        </Menu.Item>
+        <Modal trigger={signIn}>
+              <Modal.Header>Provide your Credentials</Modal.Header>
+              <Modal.Content>
+              <Form>
+                <Form.Field>
+                    <label>Email</label>
+                    <input placeholder='Enter your Email' />
+                </Form.Field>
+                <Form.Field>
+                    <label>Password</label>
+                    <input type="password" placeholder='Password Here' />
+                </Form.Field>
+                <Form.Field>
+                    <Checkbox label='I agree to the Terms and Conditions' />
+                </Form.Field>
+                <Button primary><i className="fa fa-sign-in" style={{fontSize:"20px",marginLeft:'5px'}}></i>Login</Button>
+                </Form>
+              </Modal.Content>
+            </Modal>
+      </Menu>
+    )
+  }
+}
+
+class ModalExample extends React.Component{
+    constructor(props){
+        super(props);
+    }
     render(){
         return(
-            <div class="row myNav">
-                <div class="col-md-12">
-                    <span class="heading">
-                        AuthorsDaddy 
-                    </span>
-                </div>
-            </div>
-        )
+            <Modal trigger={this.props.modalNode}>
+              <Modal.Header>Select a Photo</Modal.Header>
+              <Modal.Content>
+              <Form>
+                <Form.Field>
+                    <label>Email</label>
+                    <input placeholder='Enter your Email' />
+                </Form.Field>
+                <Form.Field>
+                    <label>Password</label>
+                    <input type="password" placeholder='Password Here' />
+                </Form.Field>
+                <Form.Field>
+                    <Checkbox label='I agree to the Terms and Conditions' />
+                </Form.Field>
+                <Button primary><i className="fa fa-sign-in" style={{fontSize:"20px",marginLeft:'5px'}}></i>Login</Button>
+                </Form>
+              </Modal.Content>
+            </Modal>
+          )
     }
+    
 }
 export default Navbar
