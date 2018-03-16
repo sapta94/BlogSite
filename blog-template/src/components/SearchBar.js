c2a72b2ee770af0365b5775ea0ba828d4f5cd31e
Saptarshi Dey <dey7.kol@gmail.com>
2018-03-16 23:31:16 +0530

    search bar component added

---
+++ blog-template/src/components/SearchBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Menu,Grid } from 'semantic-ui-react'
+
+class SearchBar extends React.Component{
+    render(){
+        return(
+            <div className="header-img">
+                <Grid columns="one">
+                    <Grid.Row>
+                    <Grid.Column>
+                        This is something
+                    </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+export default SearchBar
