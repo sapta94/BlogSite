be68e6d7c4c3e3f07e93aad2f16d85f3f90f1e5e
Saptarshi Dey <dey7.kol@gmail.com>
2018-03-18 18:20:20 +0530

    stastic component added

---
+++ blog-template/src/components/Statistics.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import { Grid } from 'semantic-ui-react'
+import Counter from 'react-counter'
+import AnimationCount from 'react-count-animation'
+import 'react-count-animation/dist/count.min.css';
+
+class Statistics extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            
+        }
+    }
+    
+
+    render(){
+        const settings = {
+            start: 0,
+            count: 1000,
+            duration: 3000,
+            decimals: 0,
+            useGroup: true,
+            animation: 'up'
+        };
+        return(
+            <div style={{background: 'linear-gradient(to bottom right, #91ada2, #05c67b)'}}>
+            <Grid container columns={3}>
+                <Grid.Row>
+                    <Grid.Column>
+                    <div class="stat-bar">
+                        <br/>
+                        <h1 className="title">Authors</h1>
+                        <div className="exam-div">
+                            <AnimationCount {...settings}/>
+                        </div>
+                    </div>
+                    </Grid.Column>
+                    <Grid.Column>
+                    <div class="stat-bar">
+                        <br/>
+                        <h1 className="title">Articles</h1>
+                        <div className="exam-div">
+                            <AnimationCount {...settings}/>
+                        </div>
+                    </div>
+                    </Grid.Column>
+                    <Grid.Column>
+                    <div class="stat-bar">
+                        <br/>
+                        <h1 className="title">Subscribers</h1>
+                        <div className="exam-div">
+                            <AnimationCount {...settings}/>
+                        </div>
+                    </div>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+            
+            </div>
+        )
+    }
+}
+
+export default Statistics
